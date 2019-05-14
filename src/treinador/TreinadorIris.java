20c9e42b594313ae7bc47ed5855beb6877f9de3f
Bruno Holanda <br.holanda@gmail.com>
2019-05-13 22:30:56 -0300

    add: diretório para armazenamento de arquivos para treinamento

--- src/treinador/TreinadorIris.java
+++ src/treinador/TreinadorIris.java
@@ -1,30 +1,33 @@
 package treinador;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import br.com.rn.neurorio.Perceptron;
+import br.com.rn.nucleo.Main;
 import br.com.rn.validador.Escada;
 
-public class TreinadorIris extends Treinador{
+public class TreinadorIris extends Treinador {
 
 	private List<double[]> conjuntoEntradasTreino, conjuntoEntradasTeste;
 	private List<Integer> saidasEsperadasTreino, saidasEsperadasTeste;
 	private int quantidadeTreino;
-	
+
 	public void treinar() throws NumberFormatException, IOException {
-		
+
 		String[] valores;
-		BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\brhol\\Desktop\\irissv.data"));
+		InputStream res = Main.class.getResourceAsStream("/files/iris.data");
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(res));
 		List<String> listaTreino = in.lines().collect(Collectors.toList());
 		int tamanhoAmostragem = (int) listaTreino.size();
 		quantidadeTreino = (tamanhoAmostragem * 80 / 100) / 2;
-		int quantidadeSetosa = 0,
-				quantidadeVersicolor = 0;
+		int quantidadeSetosa = 0, quantidadeVersicolor = 0;
 		in.close();
 		if (tamanhoAmostragem != 0) {
 			conjuntoEntradasTreino = new ArrayList<>();
@@ -58,15 +61,15 @@ public class TreinadorIris extends Treinador{
 					System.out.println("erro: " + erro);
 					perceptron.verificaErro(erro, fator, conjuntoEntradasTreino.get(i));
 				}
-				System.out.println("Saida: " + saida + "\nSaída Esperada: " + saidasEsperadasTreino.get(i) + "\nErro: " + erro
-						+ "\n=====================================");
+				System.out.println("Saida: " + saida + "\nSaída Esperada: " + saidasEsperadasTreino.get(i) + "\nErro: "
+						+ erro + "\n=====================================");
 			}
 			epocas++;
 			System.out.println("Época: " + epocas + "\n#####################################");
 		} while (!saidasOk);
 
 		System.out.println("\nÉpocas: " + epocas);
-		
+
 		int erros = 0, acertos = 0;
 		for (int i = 0; i < conjuntoEntradasTeste.size(); i++) {
 			saida = perceptron.saidaAxionio(conjuntoEntradasTeste.get(i));
@@ -78,14 +81,14 @@ public class TreinadorIris extends Treinador{
 		}
 		System.out.println("Acertos: " + acertos + "\nErros: " + erros);
 	}
-	
+
 	private int updateListas(int quantidade, String[] valores, int esperado) {
 		if (quantidade < quantidadeTreino) {
-			conjuntoEntradasTreino.add(new double[] {Double.valueOf(valores[0]), Double.valueOf(valores[2])});
+			conjuntoEntradasTreino.add(new double[] { Double.valueOf(valores[0]), Double.valueOf(valores[2]) });
 			saidasEsperadasTreino.add(esperado);
 			quantidade++;
 		} else {
-			conjuntoEntradasTeste.add(new double[] {Double.valueOf(valores[0]), Double.valueOf(valores[2])});
+			conjuntoEntradasTeste.add(new double[] { Double.valueOf(valores[0]), Double.valueOf(valores[2]) });
 			saidasEsperadasTeste.add(esperado);
 		}
 		return quantidade;
